ae579574e526144dfd0e45c80f4c0105ce2c8121
habilithm97 <habinkim97@gmail.com>
2024-05-30 17:09:13 +0900

    RecyclerView(아이템 UI, ListAdapter)

---
+++ app/src/main/java/com/example/memo2405/adapter/MemoAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.memo2405.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.memo2405.R
+import com.example.memo2405.databinding.ItemMemoBinding
+import com.example.memo2405.room.Memo
+
+/**
+ * ListAdapter : RecyclerView.Adapter를 확장한 클래스
+ -> 데이터의 변경 사항을 자동으로 처리하여 효율적으로 목록을 업데이트할 수 있도록 도와줌
+ */
+class MemoAdapter : ListAdapter<Memo, MemoAdapter.MemoViewHolder>(MemoDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemoViewHolder {
+        val binding = DataBindingUtil.inflate<ItemMemoBinding>(LayoutInflater.from(parent.context), R.layout.item_memo, parent, false)
+        return MemoViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MemoViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class MemoViewHolder(private val binding: ItemMemoBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(memo: Memo) {
+            binding.memo = memo
+        }
+    }
+
+    class MemoDiffCallback : DiffUtil.ItemCallback<Memo>() {
+        override fun areItemsTheSame(oldItem: Memo, newItem: Memo): Boolean {
+            return oldItem.id == newItem.id // 두 아이템이 같은지
+        }
+
+        override fun areContentsTheSame(oldItem: Memo, newItem: Memo): Boolean {
+            return oldItem == newItem // 두 아이템의 데이터가 같은지
+        }
+    }
+}
