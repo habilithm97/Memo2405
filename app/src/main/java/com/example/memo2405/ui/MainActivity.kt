71c1d8a0772114824840e3f25787357419f901b2
habilithm97 <habinkim97@gmail.com>
2024-06-04 16:24:24 +0900

    메모 추가하기

--- app/src/main/java/com/example/memo2405/ui/MainActivity.kt
+++ app/src/main/java/com/example/memo2405/ui/MainActivity.kt
@@ -3,10 +3,22 @@ package com.example.memo2405.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.memo2405.adapter.MemoAdapter
 import com.example.memo2405.databinding.ActivityMainBinding
+import com.example.memo2405.room.Memo
+import com.example.memo2405.ui.MemoActivity.Companion.INSERT
+import com.example.memo2405.viewmodel.MemoVM
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    private val memoAdapter by lazy { MemoAdapter() }
+    private val viewModel: MemoVM by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +31,33 @@ class MainActivity : AppCompatActivity() {
         binding.apply {
             fab.setOnClickListener {
                 val intent = Intent(this@MainActivity, MemoActivity::class.java)
-                startActivity(intent)
+                resultLauncher.launch(intent)
+            }
+            rv.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity).apply {
+                    reverseLayout = true
+                    stackFromEnd = true
+                }
+                setHasFixedSize(true) // 고정된 사이즈의 RecyclerView -> 불필요한 리소스 줄이기
+                adapter = memoAdapter
             }
         }
+
+        resultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) { result ->
+            val intent = result.data
+            if (result.resultCode == INSERT) { // 추가 모드
+                if (intent != null) {
+                    val title = intent.getStringExtra(MemoActivity.TITLE).toString()
+                    val content = intent.getStringExtra(MemoActivity.CONTENT).toString()
+                    val memo = Memo(title, content)
+                    viewModel.addMemo(memo)
+                }
+            }
+        }
+        // 리스트를 관찰하여 변경 시 어댑터에 전달함
+        viewModel.getAll.observe(this@MainActivity, Observer {
+            memoAdapter.submitList(it)
+        })
     }
 }
--- app/src/main/java/com/example/memo2405/ui/MemoActivity.kt
+++ app/src/main/java/com/example/memo2405/ui/MemoActivity.kt
@@ -1,11 +1,24 @@
 package com.example.memo2405.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.memo2405.R
 import com.example.memo2405.databinding.ActivityMemoBinding
 
 class MemoActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMemoBinding.inflate(layoutInflater) }
+    private lateinit var toast: Toast
+
+    companion object {
+        const val ID = "id"
+        const val TITLE = "title"
+        const val CONTENT = "content"
+        const val INSERT = 0
+        const val UPDATE = 1
+    }
+    private var id = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +28,43 @@ class MemoActivity : AppCompatActivity() {
     }
 
     private fun init() {
+        toast = Toast.makeText(this@MemoActivity, "", Toast.LENGTH_SHORT)
 
+        binding.apply {
+            btnCancel.setOnClickListener {
+                finish()
+            }
+            btnSave.setOnClickListener {
+                saveMemo()
+            }
+        }
+    }
+
+    private fun saveMemo() {
+        val title = binding.edtTitle.text.toString()
+        val content = binding.edtContent.text.toString()
+
+        if(title.trim().isEmpty()) {
+            shortToast(getString(R.string.empty_title))
+        } else {
+            val intent = Intent()
+            intent.putExtra(TITLE, title)
+            intent.putExtra(CONTENT, content)
+            setResult(INSERT, intent)
+            finish()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        binding.edtTitle.requestFocus()
+    }
+
+    // 토스트 메시지 중복 방지
+    private fun shortToast(msg: String) {
+        toast.cancel()
+        toast = Toast.makeText(this@MemoActivity, msg, Toast.LENGTH_SHORT)
+        toast.show()
     }
 }
